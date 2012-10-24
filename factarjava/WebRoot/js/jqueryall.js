28ae9561ef89ea7effc7e41c8b0060667f53d3c9
factar <factar@gmail.com>
2012-10-24 18:12:11 +0800

    增加表单验证

---
+++ factarjava/WebRoot/js/jqueryall.js
@@ -0,0 +1,6 @@
+var server="http://127.0.0.1:8081/";
+var appname ="factarjava";
+document.writeln("<script type=\"text/javascript\" src=\""+server+appname+"/js/jquery/jquery.min.js\"></script>")
+document.writeln("<script type=\"text/javascript\" src=\""+server+appname+"/js/jquery/Validform_v5.1_min.js\"></script>")
+
+//document.writeln
